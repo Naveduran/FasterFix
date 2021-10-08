93129b2f1bcc540d3818d3f84be8b097dc9fe550
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-10-08 11:23:06 -0500

    [UPDATE](Allowed_hosts)

--- Api/FasterFix/settings.py
+++ Api/FasterFix/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [ '3.15.141.149' ]
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [ '3.15.141.149' ]
+# ALLOWED_HOSTS = [ '*' ]
 
 # Application definition
 
