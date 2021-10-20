9211da2a89ba73b16395fb3ff2c2f59233a5ce15
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-10-20 18:07:02 -0500

    [Add] login component

--- Frontend/faster/src/views/login.js
+++ Frontend/faster/src/views/login.js
@@ -1,12 +1,37 @@
-import SelectDropdown  from "../components/selectrole";
-export default function LoginPage() {
-    return (
-        <div class="w-screen h-screen flex items-center justify-center border-4  bg-gray-100">
-            <div class="text-center bg-blue-200 mx-auto space-y-6 w-1/3 h-3/6 p-9 rounded-2xl shadow-2xl">
-                <h1 class="m-3 text-5xl">Welcome</h1>
-                <h2>Select your Role</h2>
-                <SelectDropdown/>
+import React from 'react';
+
+export default function Login() {
+  return(
+    <div class="flex h-screen items-center bg-gray-100 justify-center">
+      <div class="w-auto h-auto bg-gray-200 p-9 text-center shadow-2xl rounded-xl">
+        <h2 class="m-3 text-5xl">
+            Faster Fix
+        </h2>
+        <p>
+            Warranties Manager System
+        </p>
+        <form>
+            <label class="flex gap-x-11 my-6">
+                <p>
+                    Email:
+                </p>
+                <input type="text" name="name" class="mx-2   border-2 rounded-lg" />
+            </label>
+            <label class="flex gap-x-3">
+                <p>
+                    Password
+                </p>
+                <input type="password" name="name" class="mx-2   border-2 rounded-lg" />
+            </label>
+            <div class="mt-7">
+                <button 
+                type="submit" 
+                class="transition font-serif duration-500 bg-gray-500 hover:bg-gray-400 transform hover:-translate-y-1 hover:scale-110 rounded- py-1 px-2"
+                >Start
+                </button>
             </div>
-        </div>
-    )
+        </form>
+      </div>
+    </div>
+  )
 }
