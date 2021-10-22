9c33e6505487f6eb278c2af97541e95826e54c53
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-10-21 23:10:14 -0500

    [Add] login and landing basic views

--- Frontend/faster/src/views/landing.js
+++ Frontend/faster/src/views/landing.js
@@ -1,3 +1,54 @@
-export default function (){
-    
+import { Link } from "react-router-dom"
+export default function LandingPage(){
+    return(
+        <div class="w-screen h-screen p-2">
+            <div class=" flex justify-end pt-2 pr-2">
+                <Link to="/Login">
+                <button 
+                    class="font-bold text-lg flex justify-end bg-loginButton active:bg-colorBu py-1 px-4"
+                    >Login
+                </button>
+                </Link>
+            </div>
+            <div class="flex flex-col space-y-3 items-center">
+                    <h1>
+                        FASTER FIX
+                    </h1>
+                <div class="text-justify space-y-3 pt-9">
+                    
+                    <h2>
+                        Warranties Manager System
+                    </h2>
+                    <p class="">
+                        The PYMES tend to assign client service responsibilities centralized on only one person that receives requests from everyone. 
+                    </p>
+                    <p>
+                        It creates a bottleneck that can generate bad reputation and legal penalty fees. 
+                    </p>
+                    <p>
+                        Let's change that!
+                    </p>
+                    <div class="flex pl-5 space-x-5 pt-10">
+                        <div class="w-72 ">
+                            <ul class="list-disc space-y-2.5">
+                            <li>Acknowledge the role of every part of the company in the process</li>
+                            <li>Notify automatically the next agent when a case is needing its review</li>
+                            <li>Give relevant information to take action.</li>
+                            </ul>
+                        </div>
+                        <div class="w-30"/>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <Link to="/Register">
+                        <button 
+                            class="font-bold text-lg flex justify-end bg-loginButton active:bg-colorBu py-1 px-4"
+                            >Start Now
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            
+        </div>
+    )
 }
--- Frontend/faster/src/views/login.js
+++ Frontend/faster/src/views/login.js
@@ -4,10 +4,10 @@ export default function Login() {
   
   return(
     <div class="flex h-screen items-center bg-gray-100 justify-center">
-      <div class="w-auto h-auto bg-gray-200 p-9 text-center shadow-2xl rounded-xl">
-        <h2 class="m-3 text-5xl italic">
+      <div class="w-auto h-auto bg-loginColor p-9 text-center shadow-2xl rounded-3xl">
+        <h1 class="m-3 text-5xl italic">
             Faster Fix
-        </h2>
+        </h1>
         <p>
             Warranties Manager System
         </p>
@@ -27,7 +27,7 @@ export default function Login() {
             <div class="mt-7">
                 <button 
                 type="submit" 
-                class="transition font-serif duration-500 bg-gray-500 hover:bg-gray-400 transform hover:-translate-y-1 hover:scale-110 rounded- py-1 px-2"
+                class=" font-bold text-lg  bg-loginButton active:bg-colorBu py-1 px-4"
                 >Start
                 </button>
             </div>
