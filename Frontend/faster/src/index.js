d70a9aeb641ce02f39b1b7c04e5192b1fef68a9d
Natalia <naveduran@gmail.com>
2021-10-25 17:19:41 -0500

    [Add] mui components

--- Frontend/faster/src/index.js
+++ Frontend/faster/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppRoutes from './routes/routes';
-
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppRoutes/>
+	<StyledEngineProvider injectFirst>
+	  <AppRoutes/>
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
