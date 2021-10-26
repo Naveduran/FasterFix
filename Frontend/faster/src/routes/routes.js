044ebaf61c5fa12c8234047c166ae977cf87263f
Natalia <naveduran@gmail.com>
2021-10-25 22:42:38 -0500

    [Fix] landing page dont allowed to see other pages

--- Frontend/faster/src/routes/routes.js
+++ Frontend/faster/src/routes/routes.js
@@ -1,19 +1,22 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
+// Static Routes
 import AccountantView from "../views/accountant";
 import Autorize from "../views/autorize";
 import CaseActive from "../views/clientservice/caseactive";
-import LoginPage from "../views/login";
-import Register from "../views/register";
 import SellerView from "../views/sellerview";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 
-//new routes
+// API Integrated Routes
+import Landing from "../views/landing";
+import Register from "../views/register";
 import Active from "../views/active";
 import Done from "../views/done";
+import LoginPage from "../views/login";
 import AllActive from "../views/allActive";
 import AllDone from "../views/allDone";
 import Case from "../views/case";
@@ -23,34 +26,26 @@ import Seller from "../views/seller";
 
 
 export default function AppRoutes() {
-    return(
+    return (
         <Router>
             <Switch>
-                <Route path="/autorize">
-                    <Autorize/>
-                </Route>
-                <Route path="/detailed">
-                    <SellerView/>
-                </Route>
-                <Route path="/register">
-                    <Register/>
-                </Route>
-                <Route path="/accountant">
-                    <AccountantView/>
-                </Route>  
-                <Route path="/ActiveCSACases">
-                    <CaseActive/>
-                </Route> 
-               <Route path="/login" component="LoginPage"><LoginPage/></Route> 
-               <Route path="/register" component=""/>
-               <Route path="/active/:user_type" component="Active"/>
-               <Route path="/done/:agent_id" component="Done"/>
-               <Route path="/allActive/:criteria" component="AllActive"/>
-               <Route path="/allDone/:criteria" component="AllDone"/>
-               <Route path="/case/:request_id" component="Case"/>
-               <Route path="/create" component="Create"/>
-               <Route path="/active/:agent_id/:request_id" component="Action"/>
-               <Route path="/seller/:agent_id" component="Seller"/>
+                <Route path="/autorize"><Autorize/></Route>
+                <Route path="/detailed"><SellerView/></Route>
+                <Route path="/accountant"><AccountantView/></Route>
+                <Route path="/ActiveCSACases"><CaseActive/></Route>
+
+                <Route path="/landing"><Landing/></Route>
+                <Route path="/login"><LoginPage/></Route>
+                <Route path="/register"><Register/></Route>
+                <Route path="/create"><Create/></Route>
+
+                <Route path="/active/:user_type"><Active/></Route>
+                <Route path="/done/:agent_id"><Done/></Route>
+                <Route path="/allActive/:criteria"> <AllActive/></Route>
+                <Route path="/allDone/:criteria"><AllDone/></Route>
+                <Route path="/case/:request_id"><Case/></Route>
+                <Route path="/active/:agent_id/:request_id"><Action/></Route>
+                <Route path="/seller/:agent_id"><Seller/></Route>
             </Switch>
         </Router>
     )
