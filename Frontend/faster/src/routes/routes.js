060d91790b7f527d40f41c038ca06ccc38ade469
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-10-27 10:48:46 -0500

    [Modify] Landing route

--- Frontend/faster/src/routes/routes.js
+++ Frontend/faster/src/routes/routes.js
@@ -29,23 +29,23 @@ export default function AppRoutes() {
     return (
         <Router>
             <Switch>
-                <Route path="/autorize"><Autorize/></Route>
-                <Route path="/detailed"><SellerView/></Route>
-                <Route path="/accountant"><AccountantView/></Route>
-                <Route path="/ActiveCSACases"><CaseActive/></Route>
+                <Route exact path="/"><Landing/></Route>
+                <Route exact path="/autorize"><Autorize/></Route>
+                <Route exact path="/detailed"><SellerView/></Route>
+                <Route exact path="/accountant"><AccountantView/></Route>
+                <Route exact path="/ActiveCSACases"><CaseActive/></Route>
 
-                <Route path="/landing"><Landing/></Route>
-                <Route path="/login"><LoginPage/></Route>
-                <Route path="/register"><Register/></Route>
-                <Route path="/create"><Create/></Route>
+                <Route exact path="/login"><LoginPage/></Route>
+                <Route exact path="/register"><Register/></Route>
+                <Route exact path="/create"><Create/></Route>
 
-                <Route path="/active"><Active/></Route>
-                <Route path="/done/:agent_id"><Done/></Route>
-                <Route path="/allActive/:criteria"> <AllActive/></Route>
-                <Route path="/allDone/:criteria"><AllDone/></Route>
-                <Route path="/case/:request_id"><Case/></Route>
-                <Route path="/active/:agent_id/:request_id"><Action/></Route>
-                <Route path="/seller/:agent_id"><Seller/></Route>
+                <Route exact path="/active"><Active/></Route>
+                <Route exact path="/done/:agent_id"><Done/></Route>
+                <Route exact path="/allActive/:criteria"> <AllActive/></Route>
+                <Route exact path="/allDone/:criteria"><AllDone/></Route>
+                <Route exact path="/case/:request_id"><Case/></Route>
+                <Route exact path="/active/:agent_id/:request_id"><Action/></Route>
+                <Route exact path="/seller/:agent_id"><Seller/></Route>
             </Switch>
         </Router>
     )
