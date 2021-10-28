e4667024f55af32560b64f321cbed6b72064c65c
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-10-28 11:21:59 -0500

    [Update] styles to allActive and allDone views

--- Frontend/faster/src/views/allActive.js
+++ Frontend/faster/src/views/allActive.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+import SimpleBottomNavigation from '../components/navigation';
 const url = `http://localhost:8000/api/all/active/`
 const user_type = `datetime`
 
@@ -19,16 +19,52 @@ export default class AllActive extends React.Component {
 
   render() {
     return (
-      <table>
-      <tr>
-        <th>Case</th>
-        <th>Product</th>
-        <th>Next</th>
-        <th>Assigned on</th>
-        <th></th>
-      </tr>
-        { this.state.cases.map(c => <tr><td>{c.id}</td><td>{c.product.name}</td><td>{c.next}</td><td>{c.last_update}</td><td><button>Change Path</button></td></tr>)}
-      </table>
+      <div class="flex flex-col space-y-14 pt-14">
+        <div>
+          <SimpleBottomNavigation/>
+        </div>
+        <div class="flex justify-center items-center ">
+          <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-29 sm:px-6 lg:px-8">
+                <div className="shadow overflow-hidden border-b border-red-200 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-2">
+                    <thead className="bg-blue-400"></thead>
+          
+                      <tr>
+                        <th 
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Case(id)</th>
+                        <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Product(name)</th>
+                        <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Next</th>
+                        <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Assigned on </th>
+                        <th scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"/>
+                      </tr>
+                        { this.state.cases.map(c => 
+                          <tr>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.id}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.product.name}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.next}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.last_update}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <button 
+                              href="/active/:agent_id/:request_id:"
+                              class="rounded-lg px-2 bg-white border-double border-4 text-blue-600 border-blue-200">Change Path</button></td>
+                            </tr>)}
+                      </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
     )
   }
 }
--- Frontend/faster/src/views/allDone.js
+++ Frontend/faster/src/views/allDone.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import SimpleBottomNavigation from '../components/navigation';
 
 const url = `http://localhost:8000/api/all/done/`
 const user_type = `datetime`
@@ -19,16 +20,52 @@ export default class AllDone extends React.Component {
 
   render() {
     return (
-      <table>
-      <tr>
-        <th>Case</th>
-        <th>Product</th>
-        <th>Closed</th>
-        <th>Time</th> {/* Number of days to solve the case*/}
-        <th></th>
-      </tr>
-        { this.state.cases.map(c => <tr><td>{c.id}</td><td>{c.product.name}</td><td>{c.last_update}</td><td>{ c.last_update - c.datetime }</td><td><button>Detailed View</button></td></tr>)}
-      </table>
+      <div class="flex flex-col space-y-14 pt-14">
+        <div>
+          <SimpleBottomNavigation/>
+        </div>
+        <div class="flex justify-center items-center ">
+          <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-29 sm:px-6 lg:px-8">
+                <div className="shadow overflow-hidden border-b border-red-200 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-2">
+                    <thead className="bg-blue-400"></thead>
+          
+                      <tr>
+                        <th 
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Case(id)</th>
+                        <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Product(name)</th>
+                        <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Closed</th>
+                        <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Time</th>
+                        <th scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"/>
+                      </tr>
+                        { this.state.cases.map(c => 
+                          <tr>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.id}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.product.name}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{c.last_update}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{ c.last_update - c.datetime }</td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <button 
+                              href="/active/:agent_id/:request_id:"
+                              class="rounded-lg px-2 bg-white border-double border-4 text-blue-600 border-blue-200">Detailed View</button></td>
+                            </tr>)}
+                      </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
     )
   }
 }
