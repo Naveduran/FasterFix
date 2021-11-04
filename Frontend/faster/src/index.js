89707380898b9dfa67928665d9e2f05ec98cdea3
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-11-04 02:14:55 -0500

    [Fix] lintern warnings

--- Frontend/faster/src/index.js
+++ Frontend/faster/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppRoutes from './routes/routes';
 import { StyledEngineProvider } from '@mui/material/styles';
-import { BounceIn } from 'react-animation-wrapper';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BounceIn>
 	    <StyledEngineProvider injectFirst>
 	      <AppRoutes/>
       </StyledEngineProvider>
-    </BounceIn>
   </React.StrictMode>,
   document.getElementById('root')
 );
