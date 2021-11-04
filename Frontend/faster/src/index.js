b130041b3edbbb4069e783fafeb007bbaeded3a6
Natalia <naveduran@gmail.com>
2021-11-03 23:34:17 -0500

    [Add] transitions

--- Frontend/faster/src/index.js
+++ Frontend/faster/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppRoutes from './routes/routes';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { BounceIn } from 'react-animation-wrapper';
 
 ReactDOM.render(
   <React.StrictMode>
-	<StyledEngineProvider injectFirst>
-	  <AppRoutes/>
-    </StyledEngineProvider>
+    <BounceIn>
+	    <StyledEngineProvider injectFirst>
+	      <AppRoutes/>
+      </StyledEngineProvider>
+    </BounceIn>
   </React.StrictMode>,
   document.getElementById('root')
 );
